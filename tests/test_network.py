cfae63a7e22daad01f2bce4255a603842c7631c1
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2022-12-05 10:55:23 -0500

    Tests fix (#44)
    * python version fix
    
    * python version fix
    
    * update gitignore
    
    * mock mac address
    
    * isort
    
    * update gitignore

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,4 +1,5 @@
 """Tests for the Network module"""
+import random
 import re
 
 from src.network import Network
@@ -8,6 +9,7 @@ def test_network_init():
     success = True
     try:
         _ = Network()
+        _.mac_address = mock_mac_add()
     except Exception as e:
         print(e)
         success = False
@@ -19,6 +21,7 @@ def test_network_entries():
     json = {}
     try:
         net_obj = Network()
+        net_obj.mac_address = mock_mac_add()
         net_obj.get_network_info()
         net_obj.fill_network_info(json)
         for k, v in json.items():
@@ -38,6 +41,7 @@ def test_network_datatype():
     json = {}
     try:
         net_obj = Network()
+        net_obj.mac_address = mock_mac_add()
         net_obj.get_network_info()
         net_obj.fill_network_info(json)
     except Exception as e:
@@ -66,6 +70,9 @@ def test_macadd():
     json = {}
     try:
         net_obj = Network()
+        print(net_obj.mac_address)
+        net_obj.mac_address = mock_mac_add()
+        print(net_obj.mac_address)
         net_obj.get_network_info()
         net_obj.fill_network_info(json)
         if not re.match(
@@ -78,3 +85,11 @@ def test_macadd():
         print("Invalid Mac address validation", e)
         success = False
     assert success
+
+
+def mock_mac_add():
+    return "02:00:00:%02x:%02x:%02x" % (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+    )
