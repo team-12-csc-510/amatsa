ffb3b4bb43edc4d3990e1590d71bced1d4aa4e46
Saksham Thakur <68951147+Sakshamrzt@users.noreply.github.com>
2022-12-05 12:36:57 -0500

    File monitoring test added

---
+++ tests/test_file_monitoring.py
@@ -0,0 +1,68 @@
+from src.file_monitoring import FileMonitoring
+import os
+
+def test_file_init():
+    success = True
+    try:
+        _ = FileMonitoring()
+    except Exception as e:
+        print(e)
+        success = False
+    assert success, "Failed to initialize monitoring"
+
+
+def test_get_file():
+    success = True
+    try:
+        monitor = FileMonitoring()
+        monitor.get_filename()
+    except Exception as e:
+        print(e)
+        success = False
+    assert success, "Failed to get file"
+
+
+def test_start_file_monitoring():
+    success = True
+    try:
+        monitor = FileMonitoring()
+        monitor.start_file_monitoring()
+    except Exception as e:
+        print(e)
+        success = False
+    assert success, "Failed to start file monitoring"
+
+
+def test_read_data():
+    success = True
+    try:
+        monitor = FileMonitoring()
+        monitor.read_data()
+    except Exception as e:
+        print(e)
+        success = False
+    assert success, "Failed to read data"
+
+
+def test_read_data():
+    success = True
+    try:
+        f = open('file_monitoring_data', "w+")
+        monitor = FileMonitoring()
+        monitor.read_data()
+        os.remove('file_monitoring_data')
+    except Exception as e:
+        print(e)
+        success = False
+    assert success, "Failed to read data"
+
+
+def test_FolderMonitoring_init():
+    success = True
+    try:
+        _ = FileMonitoring.FolderMonitoring('file_monitoring_data')
+    except Exception as e:
+        print(e)
+        success = False
+    assert success, "Failed to initialize folder monitoring"
+
