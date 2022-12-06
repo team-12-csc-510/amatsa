993643fb7c7d5972e9e75052396d01eb3867ec24
Saksham Thakur <68951147+Sakshamrzt@users.noreply.github.com>
2022-12-05 20:11:43 -0500

    Change

--- tests/test_file_monitoring.py
+++ tests/test_file_monitoring.py
@@ -1,5 +1,3 @@
-import os
-
 from src.file_monitoring import FileMonitoring
 
 
@@ -22,27 +20,3 @@ def test_get_file():
         print(e)
         success = False
     assert success, "Failed to get file"
-
-
-def test_start_file_monitoring():
-    success = True
-    try:
-        monitor = FileMonitoring()
-        monitor.start_file_monitoring()
-    except Exception as e:
-        print(e)
-        success = False
-    assert success, "Failed to start file monitoring"
-
-
-def test_read_data():
-    success = True
-    try:
-        with open("file_monitoring_data", "w+"):
-            monitor = FileMonitoring()
-            monitor.read_data()
-            os.remove("file_monitoring_data")
-    except Exception as e:
-        print(e)
-        success = False
-    assert success, "Failed to read data"
